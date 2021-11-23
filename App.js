095d5e6443f33d8cedcfcc52eab49e7cd9644a66
Jonathan Perry <ts-jonathan.perry@rakuten.com>
2021-11-23 12:01:06 +0900

    Installed tailwind-rn and switched the style on App.js to use it instead

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import tw from "tailwind-rn";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw("flex-1 justify-center items-center")}>
+      <Text>WHATS going on Jonny Ramen fam!!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
