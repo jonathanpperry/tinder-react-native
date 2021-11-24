fb7a422bf3680241730c6c11af59d09a3f1afed8
Jonathan Perry <ts-jonathan.perry@rakuten.com>
2021-11-24 10:37:48 +0900

    Finished building login functionality

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import tw from "tailwind-rn";
+import { AuthProvider } from "./hooks/useAuth";
 import StackNavigator from "./StackNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <AuthProvider>
+        <StackNavigator />
+      </AuthProvider>
     </NavigationContainer>
   );
 }
