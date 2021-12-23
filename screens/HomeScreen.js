2f408f1919a09aead8c06e69cf5bd979f2c76eae
Jonathan Perry <jpper@umich.edu>
2021-12-22 21:58:32 -0500

    Finished building Header section of the home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,47 @@
 import { useNavigation } from "@react-navigation/core";
 import React from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import useAuth from "../hooks/useAuth";
+import tw from "tailwind-rn";
+import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-  const { logout } = useAuth();
-  return (
-    <View>
-      <Text>I am the Home Screen</Text>
-      <Button
-        title="Go to Chat Screen"
-        onPress={() => navigation.navigate("Chat")}
-      />
+  const { user, logout } = useAuth();
+  console.log(user);
 
-      <Button title="Logout" onPress={logout} />
-    </View>
+  return (
+    <SafeAreaView>
+      {/* Header */}
+      <View style={tw("flex-row items-center justify-between px-5")}>
+        <TouchableOpacity onPress={logout}>
+          <Image
+            style={tw("h-10 w-10 rounded-full")}
+            source={{ uri: user.photoURL }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={tw("h-14 w-14")} source={require("../logo.png")} />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate("Chat")}>
+          <Ionicons
+            name="chatbubbles-sharp"
+            size={30}
+            color="#FF5864"
+          ></Ionicons>
+        </TouchableOpacity>
+      </View>
+
+      {/* End of header */}
+    </SafeAreaView>
   );
 };
 
