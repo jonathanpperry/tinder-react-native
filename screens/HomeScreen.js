2d3e4e396e796187f6f56bb13f54c04e52adc760
Jonathan Perry <jpper@umich.edu>
2021-12-24 10:31:31 -0600

    Implemented a swiper on the home screen with 3 dummy users and styled to display image

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,24 +1,47 @@
 import { useNavigation } from "@react-navigation/core";
 import React from "react";
-import {
-  View,
-  Text,
-  Button,
-  SafeAreaView,
-  TouchableOpacity,
-  Image,
-} from "react-native";
+import { View, SafeAreaView, TouchableOpacity, Image } from "react-native";
 import useAuth from "../hooks/useAuth";
 import tw from "tailwind-rn";
 import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
+import Swiper from "react-native-deck-swiper";
+
+const DUMMY_DATA = [
+  {
+    firstName: "Elon",
+    lastName: "Musk",
+    occupation: "Software Developer",
+    photoURL:
+      "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg",
+    age: 40,
+    id: 111,
+  },
+  {
+    firstName: "Elon",
+    lastName: "Musk",
+    occupation: "Software Developer",
+    photoURL:
+      "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg",
+    age: 40,
+    id: 112,
+  },
+  {
+    firstName: "Elon",
+    lastName: "Musk",
+    occupation: "Software Developer",
+    photoURL:
+      "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg",
+    age: 40,
+    id: 113,
+  },
+];
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const { user, logout } = useAuth();
-  console.log(user);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={tw("flex-1")}>
       {/* Header */}
       <View style={tw("flex-row items-center justify-between px-5")}>
         <TouchableOpacity onPress={logout}>
@@ -39,8 +62,26 @@ const HomeScreen = () => {
           ></Ionicons>
         </TouchableOpacity>
       </View>
-
       {/* End of header */}
+
+      {/* Cards */}
+      <View style={tw("flex-1 -mt-6")}>
+        <Swiper
+          containerStyle={{ backgroundColor: "transparent" }}
+          cards={DUMMY_DATA}
+          renderCard={(card) => (
+            <View
+              key={card.id}
+              style={tw("relative bg-white h-3/4 rounded-xl")}
+            >
+              <Image
+                style={tw("absolute top-0 h-full w-full rounded-xl")}
+                source={{ uri: card.photoURL }}
+              />
+            </View>
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 };
