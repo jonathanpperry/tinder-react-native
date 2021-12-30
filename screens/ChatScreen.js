74219b7b35e1c33959978ebffda49cae1a8debad
Jonathan Perry <jpper@umich.edu>
2021-12-30 15:05:06 -0500

    Built the ChatRow section of the ChatScreen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
+import ChatList from "../components/ChatList";
 import Header from "../components/Header";
 
 const ChatScreen = () => {
   return (
     <SafeAreaView>
       <Header title="Chat" callEnabled />
-      {/* <ChatList /> */}
+      <ChatList />
     </SafeAreaView>
   );
 };
