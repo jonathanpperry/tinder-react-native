b5994425c20e3b4cc9fc76154bab0f210e0a0132
Jonathan Perry <jpper@umich.edu>
2021-12-30 14:17:14 -0500

    Built the Header section of the ChatScreen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
+import Header from "../components/Header";
 
 const ChatScreen = () => {
   return (
-    <View>
-      <Text>Chat Screen!</Text>
-    </View>
+    <SafeAreaView>
+      <Header title="Chat" callEnabled />
+      {/* <ChatList /> */}
+    </SafeAreaView>
   );
 };
 
