38cdee4f712f3e12ecaec2d393938e484c554c90
Jonathan Perry <jpper@umich.edu>
2022-01-03 22:56:54 -0500

    Finished building out the Message Screen

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,24 +1,73 @@
-import { useRoute } from '@react-navigation/native'
-import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
-import Header from '../components/Header'
-import useAuth from '../hooks/useAuth'
-import getMatchedUserInfo from '../lib/getMatchedUserInfo'
+import { useRoute } from "@react-navigation/core";
+import React, { useState } from "react";
+import {
+  FlatList,
+  Keyboard,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import Header from "../components/Header";
+import useAuth from "../hooks/useAuth";
+import getMatchedUserInfo from "../lib/getMatchedUserInfo";
+import tw from "tailwind-rn";
+import SenderMessage from "../components/SenderMessage";
+import ReceiverMessage from "../components/ReceiverMessage";
 
 const MessageScreen = () => {
-    const { user } = useAuth()
-    const { params } = useRoute()
+  const { user } = useAuth();
+  const { params } = useRoute();
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
 
-    const { matchDetails } = params
-    return (
-        <SafeAreaView>
-            <Header
-                title={getMatchedUserInfo(matchDetails?.users, user.uid).displayName}
-                callEnabled
-            />
-            <Text>Message screen</Text>
-        </SafeAreaView>
-    )
-}
+  const { matchDetails } = params;
 
-export default MessageScreen
+  const sendMessage = () => {};
+
+  return (
+    <SafeAreaView style={tw("flex-1")}>
+      <Header
+        title={getMatchedUserInfo(matchDetails?.users, user.uid).displayName}
+        callEnabled
+      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={tw("flex-1")}
+        keyboardVerticalOffset={10}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <FlatList
+            data={messages}
+            style={tw("pl-4")}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item: message }) =>
+              message.userId === user.uid ? (
+                <SenderMessage key={message.id} message={message} />
+              ) : (
+                <ReceiverMessage key={message.id} message={message} />
+              )
+            }
+          ></FlatList>
+        </TouchableWithoutFeedback>
+        <View
+          style={tw(
+            "flex-row justify-between bg-white items-center border-t border-gray-200 px-5 py-2"
+          )}
+        >
+          <TextInput
+            style={tw("h-10 text-lg")}
+            placeholder="Send Message..."
+            onChangeInput={setInput}
+            onSubmitEditing={sendMessage}
+            value={input}
+          />
+          <Button title="Send" color="#FF5864" />
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+export default MessageScreen;
